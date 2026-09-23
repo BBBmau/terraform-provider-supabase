9b41190d7657767cc5ecf54b3748b35c4068d6ec
Mauricio Alvarez Leon <alvarez.mauriciotm@gmail.com>
2026-09-22 21:56:54 -0700

    Revert "refactor: share string schema attributes across resources and data sources"
    This reverts commit b36f87d37a2b4ebe5a63001c4d79bcda0ab9acb7.

--- internal/provider/apikey_resource.go
+++ internal/provider/apikey_resource.go
@@ -16,6 +16,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/objectplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/hashicorp/terraform-plugin-log/tflog"
 	"github.com/oapi-codegen/nullable"
@@ -77,11 +78,11 @@ func (d *APIKeyResource) Schema(ctx context.Context, req resource.SchemaRequest,
 		MarkdownDescription: "API Key resource",
 
 		Attributes: map[string]schema.Attribute{
-			"id":          apiKeyIDAttribute.resource(),
+			"id":          apiKeyIDAttribute.resource(stringplanmodifier.UseStateForUnknown()),
 			"project_ref": apiKeyProjectRefAttribute.resource(),
 			"name":        apiKeyNameAttribute.resource(),
 			"description": apiKeyDescriptionAttribute.resource(),
-			"type":        apiKeyTypeAttribute.resource(),
+			"type":        apiKeyTypeAttribute.resource(stringplanmodifier.UseStateForUnknown()),
 			"api_key":     apiKeyValueAttribute.resource(),
 			"secret_jwt_template": apiKeySecretJWTTemplateResource(
 				objectplanmodifier.UseStateForUnknown(),
